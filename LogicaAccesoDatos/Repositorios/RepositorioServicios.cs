4a3bbcf0a1fc4f613647c843ec05c23ae1e2e81e
Federico Feltrini <FF68304@fi365.ort.edu.uy>
2024-03-02 20:52:33 -0300

    01 - Avances Servicios

---
+++ LogicaAccesoDatos/Repositorios/RepositorioServicios.cs
@@ -0,0 +1,45 @@
+﻿using LogicaAccesoDatos.BaseDatos;
+using LogicaAccesoDatos.Interfaces;
+using LogicaNegocio.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Repositorios
+{
+    public class RepositorioServicios:IRepositorioServicios
+    {
+        public SGMVContext Contexto { get; set; }
+        public RepositorioServicios(SGMVContext contexto)
+        {
+            Contexto = contexto;
+        }
+
+        public void Add(Servicio item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Servicio item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servicio GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Servicio> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
