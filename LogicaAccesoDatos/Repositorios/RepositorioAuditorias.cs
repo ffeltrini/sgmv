a062f329c5739d3eefa4ea3d0f9ef2a188e73a3f
Federico Feltrini <FF68304@fi365.ort.edu.uy>
2024-03-02 23:47:33 -0300

    03 - Inicio Auditorias

---
+++ LogicaAccesoDatos/Repositorios/RepositorioAuditorias.cs
@@ -0,0 +1,45 @@
+﻿using LogicaAccesoDatos.BaseDatos;
+using LogicaAccesoDatos.Interfaces;
+using LogicaNegocio.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Repositorios
+{
+    public class RepositorioAuditorias:IRepositorioAuditorias
+    {
+        public SGMVContext Contexto { get; set; }
+        public RepositorioAuditorias(SGMVContext contexto)
+        {
+            Contexto = contexto;
+        }
+
+        public void Add(Auditoria item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Auditoria item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Auditoria GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Auditoria> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicaNegocio/EntidadesNegocio/Auditoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.EntidadesNegocio
+{
+    public class Auditoria
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public DateTime FechaHora { get; set; }
+        public int IdEntidad { get; set; }
+        public string TipoEntidad { get; set; }
+    }
+}
