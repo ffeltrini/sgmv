b7f69dbebeb2030c924b638109a728cfa4488b17
Federico Feltrini <FF68304@fi365.ort.edu.uy>
2024-03-02 23:24:08 -0300

    02 - Inicio Usuario

---
+++ LogicaAccesoDatos/Repositorios/RepositorioUsuarios.cs
@@ -0,0 +1,45 @@
+﻿using LogicaAccesoDatos.BaseDatos;
+using LogicaAccesoDatos.Interfaces;
+using LogicaNegocio.EntidadesNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAccesoDatos.Repositorios
+{
+    public class RepositorioUsuarios:IRepositorioUsuarios
+    {
+        public SGMVContext Contexto { get; set; }
+        public RepositorioUsuarios(SGMVContext contexto)
+        {
+            Contexto = contexto;
+        }
+
+        public void Add(Usuario item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicaNegocio/EntidadesNegocio/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.EntidadesNegocio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
