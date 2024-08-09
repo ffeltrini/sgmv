8f8b4b3f56f0ac6ef4e470788f63559f9b68972f
Federico Feltrini <FF68304@fi365.ort.edu.uy>
2024-08-09 00:12:31 -0300

    Agregado más atributos Servicios

---
+++ LogicaAccesoDatos/Migrations/20240809000619_Servicio08.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LogicaAccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Servicio08 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Siniestro",
+                table: "ServicioMantenimientos");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProximoServicio",
+                table: "Servicios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Siniestro",
+                table: "Servicios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fin",
+                table: "ServicioMantenimientos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inicio",
+                table: "ServicioMantenimientos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Observaciones",
+                table: "ServicioMantenimientos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProximoServicio",
+                table: "Servicios");
+
+            migrationBuilder.DropColumn(
+                name: "Siniestro",
+                table: "Servicios");
+
+            migrationBuilder.DropColumn(
+                name: "Fin",
+                table: "ServicioMantenimientos");
+
+            migrationBuilder.DropColumn(
+                name: "Inicio",
+                table: "ServicioMantenimientos");
+
+            migrationBuilder.DropColumn(
+                name: "Observaciones",
+                table: "ServicioMantenimientos");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Siniestro",
+                table: "ServicioMantenimientos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
